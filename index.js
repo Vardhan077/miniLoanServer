bd0eeb84e6e1afe9928248836066e7455e143a07
Vardhan077 <vardhanmarriwada777@gmail.com>
2024-11-22 14:37:08 +0530

    ff

--- index.js
+++ index.js
@@ -10,23 +10,25 @@ const PORT = process.env.PORT || 5000;
 app.options("*", cors());
 
 app.use(express.json());
-// app.use(cors(
-//   {
-//     origin: ["https://mini-loan-app-u9nx.vercel.app/","https://miniloanapp.netlify.app/","http://localhost:3000","https://miniloanapp-6czr.onrender.com"],
-//     methods: ["POST", "GET","PUT","DELETE"],
-//     credentials: true
-//   }
-// ));
+app.use(cors(
+  {
+    origin: ["https://mini-loan-app-u9nx.vercel.app/","https://miniloanapp.netlify.app/","http://localhost:3000","https://miniloanapp-6czr.onrender.com"],
+    methods: ["POST", "GET","PUT","DELETE"],
+    credentials: true
+  }
+));
 
-app.use(cors({
-  origin: "*", // Allow all origins for debugging
-  methods: ["POST", "GET", "PUT", "DELETE"],
-  credentials: true,
-}));
+
+// app.use(cors({
+//   origin: "*", // Allow all origins for debugging
+//   methods: ["POST", "GET", "PUT", "DELETE"],
+//   credentials: true,
+// }));
 
 
 app.use("/api/user", require("./routes/user"));
 app.use("/api/loan", require("./routes/loan"));
+
 
 
 app.listen(PORT, () => {
